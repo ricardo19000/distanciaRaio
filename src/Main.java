1904e60fb613e598c75d5821a770d89675e1ad76
O Tal do Ricardo <33401083+ricardo19000@users.noreply.github.com>
2025-06-03 00:28:11 -0300

    Adição da solução 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 //Página 38 - Calcular a distância, em pés, na qual um ouvinte está de um relâmpago
 public class Main {
     public static void main(String[] args) {
+        double pes, tempo, dist;
 
+        pes = 1100.0;
+        tempo = 7.2;
+        dist = pes * tempo;
+
+        System.out.println("O raio está a " + dist + " pés");
     }
 }
